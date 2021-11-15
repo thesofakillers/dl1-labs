ffb0cac3d1f3d26708e4e3eebc20d04a5a2e06e9
Giulio Starace <giulio.starace@gmail.com>
2021-11-15 23:01:15 +0100

    implement plotting and hyperparam opt funcs

--- assignment1/compare_batch_norm.py
+++ assignment1/compare_batch_norm.py
@@ -21,12 +21,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import pickle
+
 import numpy as np
 import os
 from mlp_pytorch import MLP
 import cifar10_utils
 import train_mlp_pytorch
 
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -41,20 +44,32 @@ def train_models(results_filename):
     Note that we split the running of the model and the plotting, since you might want to
     try out different plotting configurations without re-running your models every time.
 
-    Args:
-      results_filename - string which specifies the name of the file to which the results
-                         should be saved.
-
-    TODO:
-    - Loop over all requested hyperparameter configurations and train the models accordingly.
-    - Store the results in a file. The form of the file is left up to you (numpy, json, pickle, etc.)
+    Parameters
+    ----------
+    results_filename : string
+        specifies the name of the file to which the results
+        should be saved.
     """
     #######################
     # PUT YOUR CODE HERE  #
     #######################
-    # TODO: Run all hyperparameter configurations as requested
-    results = None
-    # TODO: Save all results in a file with the name 'results_filename'. This can e.g. by a json file
+    # define our hyperparameter search space
+    dim_set = [[128], [256, 128], [512, 256, 128]]
+    bn_set = [False, True]
+    # initialize results dictionary
+    results = {bn_bool: {tuple(dims): {} for dims in dim_set} for bn_bool in bn_set}
+    # # loop over all hyperparameter configurations
+    for dims in dim_set:
+        for bn_bool in bn_set:
+            print(f"Training model with dims: {dims}, batch norm: {bn_bool}")
+            # only intereseted in logging_dict
+            _, _, _, logging_dict = train_mlp_pytorch.train(
+                dims, 0.1, bn_bool, 128, 20, 42, "data/"
+            )
+            results[bn_bool][tuple(dims)] = logging_dict
+    # # serialize results
+    with open(results_filename, "wb") as f:
+        pickle.dump(results, f)
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -64,19 +79,50 @@ def plot_results(results_filename):
     """
     Plots the results that were exported into the given file.
 
-    Args:
-      results_filename - string which specifies the name of the file from which the results
-                         are loaded.
-
-    TODO:
-    - Visualize the results in plots
-
-    Hint: you are allowed to add additional input arguments if needed.
+    Parameters
+    ----------
+    results_filename : string
+        which specifies the name of the file from which the results
+        are loaded.
     """
     #######################
     # PUT YOUR CODE HERE  #
     #######################
-    pass
+    with open(results_filename, "rb") as f:
+        results = pickle.load(f)
+    # define our hyperparameter search space (we could derive this from the dict)
+    dim_set = [[128], [256, 128], [512, 256, 128]]
+    # bn_set = [False, True]
+    fig, axes = plt.subplots(2, 3, figsize=(12, 8), sharey="row")
+    epochs = np.arange(1, 21)
+    for i, (phase, row) in enumerate(zip(("train", "validation"), axes)):
+        for dims, ax in zip(dim_set, row):
+            ax.plot(
+                epochs,
+                results[False][tuple(dims)]["accuracy"][phase],
+                label="No Batch Norm",
+                color="black" if phase == "train" else "darkblue",
+                linestyle="solid",
+            )
+            ax.plot(
+                epochs,
+                results[True][tuple(dims)]["accuracy"][phase],
+                label="With Batch Norm",
+                color="black" if phase == "train" else "darkblue",
+                linestyle="dashed",
+            )
+            ax.set_xlabel("Epoch")
+            ax.set_title(
+                f"{'Training' if phase =='train' else 'Validation'} Accuracy per Epoch"
+                f"\nHidden Layer Dimensionality: {dims}"
+            )
+        ax.legend()
+    fig.suptitle(
+        "Validation and Training Accuracy per Epoch with and without Batch"
+        " Normalization\nfor MLP's with different hidden layer dimensionalities"
+    )
+    fig.set_tight_layout(True)
+    plt.show()
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -84,7 +130,7 @@ def plot_results(results_filename):
 
 if __name__ == "__main__":
     # Feel free to change the code below as you need it.
-    FILENAME = "results.txt"
-    if not os.path.isfile(FILENAME):
-        train_models(FILENAME)
+    FILENAME = "output/compare_results.pkl"
+    # if not os.path.isfile(FILENAME):
+    #     train_models(FILENAME)
     plot_results(FILENAME)
