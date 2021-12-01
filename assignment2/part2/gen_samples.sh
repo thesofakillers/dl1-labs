316aa97f826de49c7ad4b5b67015da24b4d87967
Giulio Starace <giulio.starace@gmail.com>
2021-12-01 13:05:52 +0100

    bash script for sampling at various temperatures

---
+++ assignment2/part2/gen_samples.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# checkpoint dir
+if [ ! -z $1 ]; then
+  checkpoint_dir=$1
+else
+  checkpoint_dir="./"
+fi
+
+# text file
+if [ ! -z $2 ]; then
+  txt_file=$2
+else
+  txt_file="assets/book_EN_grimms_fairy_tails.txt"
+fi
+
+for TEMPERATURE in 0.0 0.5 1.0 2.0; do
+  echo Sampling with temperature $TEMPERATURE
+  output_file="lstm_res_t${TEMPERATURE}.txt"
+  rm -f $output_file
+  python train.py --sample --txt_file $txt_file \
+    --checkpoint_dir $checkpoint_dir > $output_file
+  echo Done
+done
