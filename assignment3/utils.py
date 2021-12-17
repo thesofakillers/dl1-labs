888e58e0d5a0fe7b4f693d142eaecd589a44f148
Giulio Starace <giulio.starace@gmail.com>
2021-12-17 17:23:10 +0100

    fix docs

--- assignment3/utils.py
+++ assignment3/utils.py
@@ -21,14 +21,22 @@ import numpy as np
 
 def sample_reparameterize(mean, std):
     """
-    Perform the reparameterization trick to sample from a distribution with the given mean and std
-    Inputs:
-        mean - Tensor of arbitrary shape and range, denoting the mean of the distributions
-        std - Tensor of arbitrary shape with strictly positive values. Denotes the standard deviation
-              of the distribution
-    Outputs:
-        z - A sample of the distributions, with gradient support for both mean and std.
-            The tensor should have the same shape as the mean and std input tensors.
+    Perform the reparameterization trick to sample
+    from a distribution with the given mean and std
+
+    Parameters
+    ----------
+    mean : torch.Tensor
+        Tensor of arbitrary shape and range, denoting the mean of the distributions
+    std : torch.Tensor
+        Tensor of arbitrary shape with strictly positive values.
+        Denotes the standard deviation of the distribution
+
+    Returns
+    -------
+    z : torch.Tensor
+        A sample of the distributions, with gradient support for both mean and std.
+        The tensor should have the same shape as the mean and std input tensors.
     """
     assert not (std < 0).any().item(), (
         "The reparameterization trick got a negative std as input. "
@@ -41,14 +49,24 @@ def sample_reparameterize(mean, std):
 
 def KLD(mean, log_std):
     """
-    Calculates the Kullback-Leibler divergence of given distributions to unit Gaussians over the last dimension.
+    Calculates the Kullback-Leibler divergence of given
+    distributions to unit Gaussians over the last dimension.
     See Section 1.3 for the formula.
-    Inputs:
-        mean - Tensor of arbitrary shape and range, denoting the mean of the distributions.
-        log_std - Tensor of arbitrary shape and range, denoting the log standard deviation of the distributions.
-    Outputs:
-        KLD - Tensor with one less dimension than mean and log_std (summed over last dimension).
-              The values represent the Kullback-Leibler divergence to unit Gaussians.
+
+    Parameters
+    ----------
+    mean : torch.Tensor
+        Tensor of arbitrary shape and range, denoting the mean of the distributions.
+    log_std : torch.Tensor
+        Tensor of arbitrary shape and range, denoting the
+        log standard deviation of the distributions.
+
+    Returns
+    -------
+    KLD : torch.Tensor
+        Tensor with one less dimension than mean and log_std
+        (summed over last dimension).
+        The values represent the Kullback-Leibler divergence to unit Gaussians.
     """
 
     KLD = None
@@ -58,12 +76,20 @@ def KLD(mean, log_std):
 
 def elbo_to_bpd(elbo, img_shape):
     """
-    Converts the summed negative log likelihood given by the ELBO into the bits per dimension score.
-    Inputs:
-        elbo - Tensor of shape [batch_size]
-        img_shape - Shape of the input images, representing [batch, channels, height, width]
-    Outputs:
-        bpd - The negative log likelihood in bits per dimension for the given image.
+    Converts the summed negative log likelihood given
+    by the ELBO into the bits per dimension score.
+
+    Parameters
+    ----------
+    elbo : torch.Tensor
+        Tensor of shape [batch_size]
+    img_shape : array-like
+        Shape of the input images, representing [batch, channels, height, width]
+
+    Returns
+    -------
+    bpd : array-like
+        The negative log likelihood in bits per dimension for the given image.
     """
     bpd = None
     raise NotImplementedError
@@ -73,15 +99,23 @@ def elbo_to_bpd(elbo, img_shape):
 @torch.no_grad()
 def visualize_manifold(decoder, grid_size=20):
     """
-    Visualize a manifold over a 2 dimensional latent space. The images in the manifold
-    should represent the decoder's output means (not binarized samples of those).
-    Inputs:
-        decoder - Decoder model such as LinearDecoder or ConvolutionalDecoder.
-        grid_size - Number of steps/images to have per axis in the manifold.
-                    Overall you need to generate grid_size**2 images, and the distance
-                    between different latents in percentiles is 1/grid_size
-    Outputs:
-        img_grid - Grid of images representing the manifold.
+    Visualize a manifold over a 2 dimensional latent space.
+    The images in the manifold should represent the decoder's
+    output means (not binarized samples of those).
+
+    Parameters
+    ----------
+    decoder : torch.nn.Module
+        Decoder model such as LinearDecoder or ConvolutionalDecoder.
+    grid_size : int
+        Number of steps/images to have per axis in the manifold.
+        Overall you need to generate grid_size**2 images, and the distance
+        between different latents in percentiles is 1/grid_size
+
+    Returns
+    -------
+    img_grid : torch.Tensor
+        Grid of images representing the manifold.
     """
 
     ## Hints:
